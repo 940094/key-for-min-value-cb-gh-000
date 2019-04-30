49cefe2f0158725c901da7ac9be740d4746ac12e
Justin Shaw <easyworkemail@gmail.com>
2019-04-30 15:45:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name = ""
+  previous_value = name_hash.collect do |name, value|
+      value # if value is lower, store the key
+  end
+  previous_value = previous_value[0]
+  puts previous_value
+  
+  name_hash.collect do |name, value|
+      if value < previous_value # if value is lower, store the key
+        
+  end
+    
 end
