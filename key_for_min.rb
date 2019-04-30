7e2472f791c7531b5da245f46d6104de3ce34b22
Justin Shaw <easyworkemail@gmail.com>
2019-04-30 15:45:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,10 +8,10 @@ def key_for_min_value(name_hash)
   end
   previous_value = previous_value[0]
   puts previous_value
-  
+
   name_hash.collect do |name, value|
       if value < previous_value # if value is lower, store the key
-        
+
   end
-    
+
 end
